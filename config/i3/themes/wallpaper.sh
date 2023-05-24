10f6634765902d3d30edd3691376da670d17bebb
Loïc Gremaud <loic.gremaud@arcanite.ch>
2023-05-24 14:14:36 +0200

    Update

---
+++ config/i3/themes/wallpaper.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+## Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
+##
+## Apply wallpaper on i3 startup
+
+WALLPAPER='/home/legrems/.config/i3/themes/gruvbox/wallpaper'
+hsetroot -root -cover "$WALLPAPER"
